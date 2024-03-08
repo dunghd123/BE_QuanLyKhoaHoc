8fd3fb089cb9465747a99a0c6911431ce0f3e3bf
dunghd123 <cutbox827@gmail.com>
2024-03-08 13:40:32 +0700

    3/8/2024 - TinhTrangHoc

--- src/main/java/com/example/jpa_final/services/TinhtranghocServices.java
+++ src/main/java/com/example/jpa_final/services/TinhtranghocServices.java
@@ -1,7 +1,74 @@
 package com.example.jpa_final.services;
 
+import com.example.jpa_final.model.BaiViet;
+import com.example.jpa_final.model.DangKyHoc;
+import com.example.jpa_final.model.TinhTrangHoc;
+import com.example.jpa_final.repo.IDangkyRep;
+import com.example.jpa_final.repo.ITinhtranghocRep;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class TinhtranghocServices {
+    @Autowired
+    ITinhtranghocRep tinhtranghocRep;
+    @Autowired
+    IDangkyRep dangkyRep;
+    ValidatorFactory valFac= Validation.buildDefaultValidatorFactory();
+    Validator val= valFac.getValidator();
+    public boolean themTinhTrangHoc(TinhTrangHoc tinhTrangHoc){
+        boolean check= false;
+        Set<ConstraintViolation<TinhTrangHoc>> violationSet= val.validate(tinhTrangHoc);
+        violationSet.forEach(x->{
+            System.out.println(x.getMessage());
+        });
+        if(violationSet.isEmpty()){
+            check=true;
+            tinhtranghocRep.save(tinhTrangHoc);
+        }
+        return check;
+    }
+    public boolean suaTinhTrangHoc(TinhTrangHoc tinhTrangHoc){
+        boolean check= false;
+        TinhTrangHoc tthCurrent= tinhtranghocRep.findById(tinhTrangHoc.getTinhtranghocID()).get();
+        tthCurrent.setTentinhtrang(tinhTrangHoc.getTentinhtrang());
+        Set<ConstraintViolation<TinhTrangHoc>> violationSet= val.validate(tthCurrent);
+        violationSet.forEach(x->{
+            System.out.println(x.getMessage());
+        });
+        if(violationSet.isEmpty()){
+            check=true;
+            tinhtranghocRep.save(tthCurrent);
+        }
+        return check;
+    }
+    public boolean xoaTinhTrangHoc(int tthid){
+        boolean check= true;
+        Optional<TinhTrangHoc> op= Optional.empty();
+        if(tinhtranghocRep.findById(tthid)==op){
+            check=false;
+        }else {
+            for(DangKyHoc dk: dangkyRep.findAll()){
+                if(dk.getTinhTrangHoc().getTinhtranghocID()==tthid){
+                    dangkyRep.delete(dk);
+                }
+            }
+            tinhtranghocRep.deleteById(tthid);
+        }
+        return check;
+    }
+    public List<TinhTrangHoc> hienThiDS(int pagenum){
+        Pageable pageable= PageRequest.of(pagenum,20);
+        return tinhtranghocRep.findAllBy(pageable);
+    }
 }
